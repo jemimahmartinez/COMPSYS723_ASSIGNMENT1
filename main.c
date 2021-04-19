a89b88d75308542ffbc607885adb036662cbf8ef
jemimahmartinez <martinezjemimah@gmail.com>
2021-04-20 11:17:47 +1200

    Resolved another merge conflict

--- main.c
+++ main.c
@@ -175,108 +175,13 @@ void button_isr(void *context, alt_u32 id)
 //	}
 //}
 
-<<<<<<< HEAD
 void freq_analyser_isr(void *context, alt_u32 id)
 {
 }
-=======
+
 void LEDHandlerTask(void)
 {
 }
-
-void freq_analyser_isr(void *context, alt_u32 id)
-{
-}
-
-// The following test prints out status information every 3 seconds.
-void print_status_task(void *pvParameters)
-{
-	while (1)
-	{
-		vTaskDelay(3000);
-		printf("****************************************************************\n");
-		printf("Hello From FreeRTOS Running on NIOS II.  Here is the status:\n");
-		printf("\n");
-		printf("The number of messages sent by the send_task:         %d\n", number_of_messages_sent);
-		printf("\n");
-		printf("The number of messages received by the receive_task1: %d\n", number_of_messages_received_task1);
-		printf("\n");
-		printf("The number of messages received by the receive_task2: %d\n", number_of_messages_received_task2);
-		printf("\n");
-		printf("The shared resource is owned by: %s\n", &sem_owner_task_name[0]);
-		printf("\n");
-		printf("The Number of times getsem_task1 acquired the semaphore %d\n", getsem_task1_got_sem);
-		printf("\n");
-		printf("The Number of times getsem_task2 acquired the semaphore %d\n", getsem_task2_got_sem);
-		printf("\n");
-		printf("****************************************************************\n");
-		printf("\n");
-	}
-}
-
-// The next two task compete for a shared resource via a semaphore.  The name of
-// the task that owns the semaphore is copied into the global variable
-// sem_owner_task_name[].
-void getsem_task1(void *pvParameters)
-{
-	while (1)
-	{
-		xSemaphoreTake(shared_resource_sem, portMAX_DELAY);
-		// block forever until receive the mutex
-		strcpy(&sem_owner_task_name[0], "getsem_task1");
-		getsem_task1_got_sem++;
-		xSemaphoreGive(shared_resource_sem);
-		vTaskDelay(100);
-	}
-}
-
-void getsem_task2(void *pvParameters)
-{
-	while (1)
-	{
-		xSemaphoreTake(shared_resource_sem, portMAX_DELAY);
-		// block forever until receive the mutex
-		strcpy(&sem_owner_task_name[0], "getsem_task2");
-		getsem_task2_got_sem++;
-		xSemaphoreGive(shared_resource_sem);
-		vTaskDelay(130);
-	}
-}
-
-// The following task fills up a message queue with incrementing data.  The data
-// is not actually used by the application.  If the queue is full the task is
-// suspended for 1 second.
-void send_task(void *pvParameters)
-{
-	unsigned int msg = 0;
-	while (1)
-	{
-		if (xQueueSend(msgqueue, (void *)&msg, 0) == pdPASS)
-		{
-			// in the message queue
-			msg++;
-			number_of_messages_sent++;
-		}
-		else
-		{
-			vTaskDelay(1000);
-		}
-	}
-}
-
-// The next two task pull messages in the queue at different rates.  The number
-// of messages received by the task is incremented when a new message is received
-void receive_task1(void *pvParameters)
-{
-	unsigned int *msg;
-	while (1)
-	{
-		xQueueReceive(msgqueue, &msg, portMAX_DELAY);
-		number_of_messages_received_task1++;
-		vTaskDelay(333);
-	}
-}
->>>>>>> 2d9465c883c2ebe3a009f1e9078a0b38c76bcb5d
 
 {
 	unsigned int *msg;
