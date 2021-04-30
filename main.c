5ed7ab432a1c590cb004046cb3e1b04816375bb5
jemimahmartinez <martinezjemimah@gmail.com>
2021-04-30 12:30:39 +1200

    Trying to figure out VGA stuff

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <unistd.h> //for usleep
 #include "system.h"
+#include "sys/alt_irq.h"
 
 // Scheduler includes
 #include "freertos/FreeRTOS.h"
@@ -120,80 +121,80 @@ int initOSDataStructs(void);
 int initCreateTasks(void);
 
 // VGA Display Task
-//void VGADisplayTask(void *pvParameters) {
-//	//initialize VGA controllers
-//	alt_up_pixel_buffer_dma_dev *pixel_buf;
-//	pixel_buf = alt_up_pixel_buffer_dma_open_dev(VIDEO_PIXEL_BUFFER_DMA_NAME);
-//	if(pixel_buf == NULL){
-//		printf("can't find pixel buffer device\n");
-//	}
-//	alt_up_pixel_buffer_dma_clear_screen(pixel_buf, 0);
-//
-//	alt_up_char_buffer_dev *char_buf;
-//	char_buf = alt_up_char_buffer_open_dev("/dev/video_character_buffer_with_dma");
-//	if(char_buf == NULL){
-//		printf("can't find char buffer device\n");
-//	}
-//	alt_up_char_buffer_clear(char_buf);
-//
-//
-//
-//	//Set up plot axes
-//	alt_up_pixel_buffer_dma_draw_hline(pixel_buf, 100, 590, 200, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
-//	alt_up_pixel_buffer_dma_draw_hline(pixel_buf, 100, 590, 300, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
-//	alt_up_pixel_buffer_dma_draw_vline(pixel_buf, 100, 50, 200, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
-//	alt_up_pixel_buffer_dma_draw_vline(pixel_buf, 100, 220, 300, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
-//
-//	alt_up_char_buffer_string(char_buf, "Frequency(Hz)", 4, 4);
-//	alt_up_char_buffer_string(char_buf, "52", 10, 7);
-//	alt_up_char_buffer_string(char_buf, "50", 10, 12);
-//	alt_up_char_buffer_string(char_buf, "48", 10, 17);
-//	alt_up_char_buffer_string(char_buf, "46", 10, 22);
-//
-//	alt_up_char_buffer_string(char_buf, "df/dt(Hz/s)", 4, 26);
-//	alt_up_char_buffer_string(char_buf, "60", 10, 28);
-//	alt_up_char_buffer_string(char_buf, "30", 10, 30);
-//	alt_up_char_buffer_string(char_buf, "0", 10, 32);
-//	alt_up_char_buffer_string(char_buf, "-30", 9, 34);
-//	alt_up_char_buffer_string(char_buf, "-60", 9, 36);
-//
-//
-////	double freq[100], dfreq[100];
-//	int i = 99, j = 0;
-//	Line line_freq, line_roc;
-//
-//	while(1){
-//
-//		//clear old graph to draw new graph
-//		alt_up_pixel_buffer_dma_draw_box(pixel_buf, 101, 0, 639, 199, 0, 0);
-//		alt_up_pixel_buffer_dma_draw_box(pixel_buf, 101, 201, 639, 299, 0, 0);
-//
-//		for(j=0;j<99;++j){ //i here points to the oldest data, j loops through all the data to be drawn on VGA
-//			if (((int)(freqThre[(i+j)%100]) > MIN_FREQ) && ((int)(freqThre[(i+j+1)%100]) > MIN_FREQ)){
-//				//Calculate coordinates of the two data points to draw a line in between
-//				//Frequency plot
-//				line_freq.x1 = FREQPLT_ORI_X + FREQPLT_GRID_SIZE_X * j;
-//				line_freq.y1 = (int)(FREQPLT_ORI_Y - FREQPLT_FREQ_RES * (freqThre[(i+j)%100] - MIN_FREQ));
-//
-//				line_freq.x2 = FREQPLT_ORI_X + FREQPLT_GRID_SIZE_X * (j + 1);
-//				line_freq.y2 = (int)(FREQPLT_ORI_Y - FREQPLT_FREQ_RES * (freqThre[(i+j+1)%100] - MIN_FREQ));
-//
-//				//Frequency RoC plot
-//				line_roc.x1 = ROCPLT_ORI_X + ROCPLT_GRID_SIZE_X * j;
-//				line_roc.y1 = (int)(ROCPLT_ORI_Y - ROCPLT_ROC_RES * freqROC[(i+j)%100]);
-//
-//				line_roc.x2 = ROCPLT_ORI_X + ROCPLT_GRID_SIZE_X * (j + 1);
-//				line_roc.y2 = (int)(ROCPLT_ORI_Y - ROCPLT_ROC_RES * freqROC[(i+j+1)%100]);
-//
-//				//Draw
-//				alt_up_pixel_buffer_dma_draw_line(pixel_buf, line_freq.x1, line_freq.y1, line_freq.x2, line_freq.y2, 0x3ff << 0, 0);
-//				alt_up_pixel_buffer_dma_draw_line(pixel_buf, line_roc.x1, line_roc.y1, line_roc.x2, line_roc.y2, 0x3ff << 0, 0);
-//			}
-//		}
-//		vTaskDelay(10);
-//
-//	}
-//}
+void VGADisplayTask(void *pvParameters) {
+	//initialize VGA controllers
+	alt_up_pixel_buffer_dma_dev *pixel_buf;
+	pixel_buf = alt_up_pixel_buffer_dma_open_dev(VIDEO_PIXEL_BUFFER_DMA_NAME);
+	if(pixel_buf == NULL){
+		printf("can't find pixel buffer device\n");
+	}
+	alt_up_pixel_buffer_dma_clear_screen(pixel_buf, 0);
+
+	alt_up_char_buffer_dev *char_buf;
+	char_buf = alt_up_char_buffer_open_dev("/dev/video_character_buffer_with_dma");
+	if(char_buf == NULL){
+		printf("can't find char buffer device\n");
+	}
+	alt_up_char_buffer_clear(char_buf);
+
+
+
+	//Set up plot axes
+	alt_up_pixel_buffer_dma_draw_hline(pixel_buf, 100, 590, 200, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
+	alt_up_pixel_buffer_dma_draw_hline(pixel_buf, 100, 590, 300, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
+	alt_up_pixel_buffer_dma_draw_vline(pixel_buf, 100, 50, 200, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
+	alt_up_pixel_buffer_dma_draw_vline(pixel_buf, 100, 220, 300, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
+
+	alt_up_char_buffer_string(char_buf, "Frequency(Hz)", 4, 4);
+	alt_up_char_buffer_string(char_buf, "52", 10, 7);
+	alt_up_char_buffer_string(char_buf, "50", 10, 12);
+	alt_up_char_buffer_string(char_buf, "48", 10, 17);
+	alt_up_char_buffer_string(char_buf, "46", 10, 22);
+
+	alt_up_char_buffer_string(char_buf, "df/dt(Hz/s)", 4, 26);
+	alt_up_char_buffer_string(char_buf, "60", 10, 28);
+	alt_up_char_buffer_string(char_buf, "30", 10, 30);
+	alt_up_char_buffer_string(char_buf, "0", 10, 32);
+	alt_up_char_buffer_string(char_buf, "-30", 9, 34);
+	alt_up_char_buffer_string(char_buf, "-60", 9, 36);
+
+
+//	double freq[100], dfreq[100];
+	int i = 99, j = 0;
+	Line line_freq, line_roc;
+
+	while(1){
+
+		//clear old graph to draw new graph
+		alt_up_pixel_buffer_dma_draw_box(pixel_buf, 101, 0, 639, 199, 0, 0);
+		alt_up_pixel_buffer_dma_draw_box(pixel_buf, 101, 201, 639, 299, 0, 0);
+
+		for(j=0;j<99;++j){ //i here points to the oldest data, j loops through all the data to be drawn on VGA
+			if (((int)(freqThre[(i+j)%100]) > MIN_FREQ) && ((int)(freqThre[(i+j+1)%100]) > MIN_FREQ)){
+				//Calculate coordinates of the two data points to draw a line in between
+				//Frequency plot
+				line_freq.x1 = FREQPLT_ORI_X + FREQPLT_GRID_SIZE_X * j;
+				line_freq.y1 = (int)(FREQPLT_ORI_Y - FREQPLT_FREQ_RES * (freqThre[(i+j)%100] - MIN_FREQ));
+
+				line_freq.x2 = FREQPLT_ORI_X + FREQPLT_GRID_SIZE_X * (j + 1);
+				line_freq.y2 = (int)(FREQPLT_ORI_Y - FREQPLT_FREQ_RES * (freqThre[(i+j+1)%100] - MIN_FREQ));
+
+				//Frequency RoC plot
+				line_roc.x1 = ROCPLT_ORI_X + ROCPLT_GRID_SIZE_X * j;
+				line_roc.y1 = (int)(ROCPLT_ORI_Y - ROCPLT_ROC_RES * freqROC[(i+j)%100]);
+
+				line_roc.x2 = ROCPLT_ORI_X + ROCPLT_GRID_SIZE_X * (j + 1);
+				line_roc.y2 = (int)(ROCPLT_ORI_Y - ROCPLT_ROC_RES * freqROC[(i+j+1)%100]);
+
+				//Draw
+				alt_up_pixel_buffer_dma_draw_line(pixel_buf, line_freq.x1, line_freq.y1, line_freq.x2, line_freq.y2, 0x3ff << 0, 0);
+				alt_up_pixel_buffer_dma_draw_line(pixel_buf, line_roc.x1, line_roc.y1, line_roc.x2, line_roc.y2, 0x3ff << 0, 0);
+			}
+		}
+		vTaskDelay(10);
+
+	}
+}
 
 // Switch Polling Task
 void SwitchPollingTask(void *pvParameters)
